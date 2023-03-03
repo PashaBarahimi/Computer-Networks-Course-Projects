609c934d48efcfdf583f0852243a1ed21d7cd4cc
Pasha Barahimi <pashabarahimi@gmail.com>
2023-03-03 11:45:10 +0330

    Fix logger comments

--- CA1 - Socket Programming/src/logger.hpp
+++ CA1 - Socket Programming/src/logger.hpp
@@ -1,13 +1,13 @@
 #ifndef LOGGER_HPP_INCLUDE
 #define LOGGER_HPP_INCLUDE
 
-#include <ostream>
 #include <fstream>
+#include <ostream>
 #include <unordered_map>
 
 class Logger {
 public:
-    enum class Level : short {
+    enum class Level {
         Info,
         Warning,
         Error
@@ -16,18 +16,35 @@ public:
     Logger(Level level);
     Logger(Level level, std::ofstream& file);
 
-    void info(const std::string& message, const std::string& messageCode = "", const std::string& action = "", const std::unordered_map<std::string, std::string>& details = {});
-    void warn(const std::string& message, const std::string& messageCode = "", const std::string& action = "", const std::unordered_map<std::string, std::string>& details = {});
-    void error(const std::string& message, const std::string& messageCode = "", const std::string& action = "", const std::unordered_map<std::string, std::string>& details = {});
+    void info(const std::string& message,
+              const std::string& messageCode = "",
+              const std::string& action = "",
+              const std::unordered_map<std::string, std::string>& details = {});
+    void warn(const std::string& message,
+              const std::string& messageCode = "",
+              const std::string& action = "",
+              const std::unordered_map<std::string, std::string>& details = {});
+    void error(const std::string& message,
+               const std::string& messageCode = "",
+               const std::string& action = "",
+               const std::unordered_map<std::string, std::string>& details = {});
 
 private:
     Level level_;
     std::ostream& stream_;
     bool isaTTY_;
 
-    void log(Level level, const std::string& message, const std::string& messageCode, const std::string& action, const std::unordered_map<std::string, std::string>& details);
-    void logJson(Level level, const std::string& message, const std::string& messageCode, const std::string& action, const std::unordered_map<std::string, std::string>& details);
-    std::string levelToString(Level level, bool color = false);
+    void log(Level level,
+             const std::string& message,
+             const std::string& messageCode,
+             const std::string& action,
+             const std::unordered_map<std::string, std::string>& details);
+    void logJson(Level level,
+                 const std::string& message,
+                 const std::string& messageCode,
+                 const std::string& action,
+                 const std::unordered_map<std::string, std::string>& details);
+    std::string levelToString(Level level, bool isJson = false);
 };
 
 #endif // LOGGER_HPP_INCLUDE
