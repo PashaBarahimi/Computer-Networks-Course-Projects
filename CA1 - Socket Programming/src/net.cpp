030c245416d80967f4282c09118054e0b5aa181c
MisaghM <misagh.mhgg@gmail.com>
2023-03-11 20:50:32 +0330

    Add POSIX socket wrapper

--- CA1 - Socket Programming/src/net.cpp
+++ CA1 - Socket Programming/src/net.cpp
@@ -1,5 +1,8 @@
 #include "net.hpp"
 
+#include <netinet/in.h>
+#include <unistd.h>
+
 #include <regex>
 #include <sstream>
 
@@ -61,6 +64,122 @@ void IpAddr::fromInt(unsigned int addr) {
     for (int i = 0; i < 4; ++i) {
         addr_[i] = (addr >> ((3 - i) * 8)) & 0xFF;
     }
+}
+
+Socket::Socket(Type type)
+    : type_(type) {
+    socket_ = socket(PF_INET, static_cast<int>(type_), 0);
+    if (type_ == Type::stream) {
+        int reuseaddr = 1;
+        setsockopt(socket_, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr));
+    }
+}
+
+Socket::~Socket() {
+    if (socket_ != -1) {
+        close(socket_);
+    }
+}
+
+bool Socket::bind(IpAddr addr, Port port) {
+    sockaddr_in addrIn = ipToSockaddr(addr, port);
+    int res = ::bind(socket_, reinterpret_cast<sockaddr*>(&addrIn), sizeof(addrIn));
+    if (res == -1) {
+        return false;
+    }
+    addr_ = addr;
+    port_ = port;
+    status_ = Status::bound;
+    return true;
+}
+
+bool Socket::listen(int queueSize) {
+    int res = ::listen(socket_, queueSize);
+    if (res == -1) {
+        return false;
+    }
+    status_ = Status::listening;
+    return true;
+}
+
+bool Socket::connect(IpAddr addr, Port port) {
+    sockaddr_in addrIn = ipToSockaddr(addr, port);
+    int res = ::connect(socket_, reinterpret_cast<sockaddr*>(&addrIn), sizeof(addrIn));
+    if (res == -1) {
+        return false;
+    }
+    addr_ = addr;
+    port_ = port;
+    status_ = Status::connected;
+    return true;
+}
+
+bool Socket::accept(Socket& outSocket) {
+    sockaddr_in clientAddrIn{};
+    socklen_t addrSize = sizeof(clientAddrIn);
+    int res = ::accept(socket_, reinterpret_cast<sockaddr*>(&clientAddrIn), &addrSize);
+    if (res == -1) {
+        return false;
+    }
+    outSocket.socket_ = res;
+    sockaddrToIp(clientAddrIn, outSocket.addr_, outSocket.port_);
+    outSocket.status_ = Status::connected;
+    return true;
+}
+
+bool Socket::send(const std::string& data) {
+    int res = ::send(socket_, data.c_str(), data.size(), 0);
+    if (res == -1) {
+        return false;
+    }
+    return true;
+}
+
+bool Socket::receive(std::string& data) {
+    std::array<char, 1024> buf;
+
+    fd_set readSet;
+    FD_ZERO(&readSet);
+    FD_SET(socket_, &readSet);
+    timeval instantTimeout{0, 0};
+
+    while (true) {
+        int receivedBytes = ::recv(socket_, &buf, buf.size(), 0);
+        if (receivedBytes == -1) {
+            return false;
+        }
+        if (receivedBytes == 0) {
+            break;
+        }
+
+        data.append(buf.data(), receivedBytes);
+        if (receivedBytes != static_cast<int>(buf.size())) {
+            break;
+        }
+
+        select(socket_ + 1, &readSet, nullptr, nullptr, &instantTimeout);
+        if (!FD_ISSET(socket_, &readSet)) {
+            break;
+        }
+    }
+    return true;
+}
+
+bool Socket::operator==(const Socket& rhs) const { return socket_ == rhs.socket_; }
+bool Socket::operator!=(const Socket& rhs) const { return socket_ != rhs.socket_; }
+
+sockaddr_in Socket::ipToSockaddr(IpAddr addr, Port port) {
+    sockaddr_in addrIn{};
+    addrIn.sin_family = AF_INET;
+    addrIn.sin_port = hton(port);
+    addrIn.sin_addr.s_addr = hton(addr.toInt());
+    return addrIn;
+}
+
+void Socket::sockaddrToIp(sockaddr_in addrIn, IpAddr& outAddr, Port& outPort) {
+    auto addrInt = ntoh(addrIn.sin_addr.s_addr);
+    outAddr = IpAddr(addrInt);
+    outPort = ntoh(addrIn.sin_port);
 }
 
 } // namespace net
--- CA1 - Socket Programming/src/net.hpp
+++ CA1 - Socket Programming/src/net.hpp
@@ -1,10 +1,14 @@
 #ifndef NETWORK_HPP_INCLUDE
 #define NETWORK_HPP_INCLUDE
 
+#include <sys/socket.h>
+
 #include <array>
 #include <string>
 
 #include "endian.hpp"
+
+struct sockaddr_in;
 
 namespace net {
 
@@ -54,6 +58,49 @@ public:
 
 private:
     std::array<unsigned short, 4> addr_{};
+};
+
+class Socket {
+public:
+    enum class Type {
+        unused = -1,
+        stream = SOCK_STREAM,
+        datagram = SOCK_DGRAM
+    };
+
+    enum class Status {
+        unused,
+        bound,
+        listening,
+        connected
+    };
+
+    Socket() = default;
+    explicit Socket(Type type);
+    ~Socket();
+
+    bool bind(IpAddr addr, Port port);
+    bool listen(int queueSize);
+    bool connect(IpAddr addr, Port port);
+    bool accept(Socket& outSocket);
+
+    bool send(const std::string& data);
+    bool receive(std::string& data);
+
+    bool operator==(const Socket& rhs) const;
+    bool operator!=(const Socket& rhs) const;
+
+private:
+    Type type_ = Type::unused;
+    IpAddr addr_ = {};
+    Port port_ = 0;
+    Status status_ = Status::unused;
+    int socket_ = -1;
+
+    friend class Select;
+
+    static sockaddr_in ipToSockaddr(IpAddr addr, Port port);
+    static void sockaddrToIp(sockaddr_in addrIn, IpAddr& outAddr, Port& outPort);
 };
 
 } // namespace net
