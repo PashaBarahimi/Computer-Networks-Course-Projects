b27679c6bcb840010bff599041dc80642b411f0b
Pasha Barahimi <pashabarahimi@gmail.com>
2023-04-19 20:01:30 +0330

    Add mappings and client send messsage

--- CA2 - Wireless LAN Simulation/ns-3-dev/scratch/main.cc
+++ CA2 - Wireless LAN Simulation/ns-3-dev/scratch/main.cc
@@ -1,5 +1,7 @@
+#include "constants.hpp"
 #include "network.hpp"
 #include "ns3/core-module.h"
+#include "utils.hpp"
 
 int main(int argc, char* argv[]) {
     bool verbose = consts::VERBOSE_DEFAULT;
@@ -7,7 +9,13 @@ int main(int argc, char* argv[]) {
     cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
     cmd.Parse(argc, argv);
 
-    Network net(verbose);
+    std::string chars = consts::VALID_CHARACTERS;
+    if (consts::SHUFFLE_MAPPINGS)
+        chars = utils::shuffle(chars);
+    auto clientSendData = utils::reverseMap(chars, consts::MESSAGE);
+    auto mappings = utils::partitionMappings(chars, consts::MAPPERS_COUNT);
+
+    Network net(verbose, clientSendData, mappings);
     net.Simulate();
 
     return 0;
