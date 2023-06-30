d4e0759029de0abb973ec5156ef1d117891d4e6a
Pasha Barahimi <pashabarahimi@gmail.com>
2023-06-30 23:58:11 +0330

    Modify main

--- CA4 - TCP Congestion Control/src/main.cpp
+++ CA4 - TCP Congestion Control/src/main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+
+#include "bbr.hpp"
+#include "new_reno.hpp"
+#include "reno.hpp"
+
 int main() {
+    std::vector<int> data(100000);
+    for (unsigned i = 0; i < data.size(); ++i) {
+        data[i] = i;
+    }
+
+    Reno reno(data);
+    reno.run(std::cout);
     return 0;
 }
